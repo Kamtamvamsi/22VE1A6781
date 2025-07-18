9ae62b63130077b2efd0cfb8b442c42c4446f62a
pheonixinc1 <pheonixinc1@gmail.com>
2025-07-18 13:46:26 +0530

    upd log

--- Frontend/log.js
+++ Frontend/log.js
@@ -1,9 +1,9 @@
 import { Log } from './log';
 
-// Log an error in a handler
+
 Log("backend", "error", "handler", "received string, expected bool");
 
-// Log a fatal error in the db layer
+
 Log("backend", "fatal", "db", "Critical database connection failure.");
 
 export async function Log(stack, level, pkg, message) {
