8d82d28cd69bb8410801a6ca9b0bbb63c8b31a90
pheonixinc1 <pheonixinc1@gmail.com>
2025-07-18 11:58:43 +0530

    upd app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './analytics'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <div className="heading">
+      <h1>Welcome to the URl shortner</h1>
+    </div>
+    <div className="bth-url">
+      <div className="url-input">
+        <input type="text" />
+      </div>
+      <button className="btn">Shorten URL</button> 
+    </div>
     </>
   )
 }
